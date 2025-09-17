63f7a82301ff97e5efb1cbfa57a1e074fec9e62c
robevert <robevert@gmail.com>
2025-09-16 21:05:24 -0500

    clock and circleshape

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import pygame
 from constants import *
 
 pygame.init()
+Clock = pygame.time.Clock()
 
 def main():
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        # last line of loop, sets fps
+        dt = Clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
